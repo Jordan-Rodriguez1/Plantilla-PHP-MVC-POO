0cb2012f61ab21b24b1336f2192b1f8bb432db64
Jordan Rodriguez <miguel20010807@gmail.com>
2023-08-11 12:08:24 -0600

    Plantilla

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+    const BASE_URL = "http://localhost/Enfermeria/";
+    const HOST = "localhost";
+    const BD = "enfermeria";
+    const DB_USER = "root";
+    const PASS = "";
+    const CHARSET = "charset=utf8";
+?>
